6d8e62def40ba7bf20c75696f943709de3fd4b1c
KYROLOS <kmagdy@gt.com.sa>
2024-01-22 16:39:26 +0200

    solving testing bug

--- index.ts
+++ index.ts
@@ -1,11 +1,10 @@
 export const fizzBuzz = (n: number, limit: number): string[] => {
   let answer: string[] = [];
-
   for (let i = n; i < limit; i++) {
     if (i % 3 === 0 && i % 5 === 0) {
       answer.push("FizzBuzz");
     } else if (i % 3 === 0) {
-      answer.push("Fizz");
+      answer.push("Fizzs");
     } else if (i % 5 === 0) {
       answer.push("Buzz");
     }
